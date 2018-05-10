41a29ad32ebffe8b68ffc00ee1063e8684bbaadf
Brandon Dixon <brandon.dixon@sevun.com>
2018-05-10 03:57:19 +0000

    added file play, but still needs to select file

--- software/software.ino
+++ software/software.ino
@@ -55,6 +55,10 @@ HardwareSerial Serial2(2);
 // Timing Functions
 //******************************
 
+time_t nextTime;
+String nextTrack;
+bool findNextFlag = true;
+
 //******************************
 // GPS Functions
 //******************************
@@ -72,6 +76,9 @@ Adafruit_GPS GPS(&GPSSerial);
 // Jason Buffer Size
 #define JSON_BUFFER_SIZE   512
 
+// Setup array to store JSON data
+char jsonText[MAX_FILE_SIZE];
+
 //******************************
 // SD Functions
 //******************************
@@ -82,6 +89,19 @@ Adafruit_GPS GPS(&GPSSerial);
 //******************************
 // MP3 Functions
 //******************************
+
+// These are the pins not used
+#define VS1053_RESET   -1     // VS1053 reset pin (not used!)
+
+// Feather ESP32 specific pins
+#define VS1053_CS      32     // VS1053 chip select pin (output)
+#define VS1053_DCS     33     // VS1053 Data/command select pin (output)
+#define CARDCS         14     // Card chip select pin
+#define VS1053_DREQ    15     // VS1053 Data request, ideally an Interrupt pin
+
+// setup the music player
+Adafruit_VS1053_FilePlayer musicPlayer = 
+  Adafruit_VS1053_FilePlayer(VS1053_RESET, VS1053_CS, VS1053_DCS, VS1053_DREQ, CARDCS);
 
 //******************************
 // Time Functions
@@ -108,8 +128,7 @@ void onTimerInterrupt()
 }
 
 void setup()
-{ 
-
+{
   //******************************
   // GPIO Setup
   //******************************
@@ -168,7 +187,21 @@ void setup()
   
   //******************************
   // MP3 Setup
-  //******************************
+  //******************************  
+  
+  // Initialise the music player
+  if ( musicPlayer.begin() )
+  {
+    Serial.print("\r\nFound the VS1053...");
+  }
+  else
+  {
+     Serial.print("\r\nCouldn't find VS1053, do you have the right pins defined?");
+     while (1);
+  }  
+  
+  // Set volume for left, right channels. Lower numbers = Louder volume!
+  musicPlayer.setVolume(10,10);
   
   //******************************
   // Time Setup
@@ -192,85 +225,98 @@ void setup()
   
   //******************************
   // Main Code
-  //******************************
-
-  // Get data from SD card, make sure file is not bigger than
-  //  the MAX_FILE_SIZE
-  char jsonText[MAX_FILE_SIZE];
-  File myFile = SD.open("/config.json");
-
-  int myFileSize = myFile.size();
-  if ( myFileSize > MAX_FILE_SIZE)
-  {
-    Serial.print("\r\nFILE SIZE IS TOO LARGE!!!");
-    Serial.print("\r\nMAX FILE SIZE IS ");
-    Serial.print(MAX_FILE_SIZE);
-    Serial.print(" AND FILE SIZE IS ");
-    Serial.print(myFileSize);
-    while(1);  // don't do anything more    
-  }
-  
-  myFile.readBytes(jsonText, MAX_FILE_SIZE);
-  myFile.close();  
-
-  // Memory pool for JSON object tree.
-  //
-  // Inside the brackets is the size of the pool in bytes.
-  // Don't forget to change this value to match your JSON document.
-  // Use arduinojson.org/assistant to compute the capacity.
-  StaticJsonBuffer<JSON_BUFFER_SIZE> jsonBuffer;
-
-  // Root of the object tree.
-  //
-  // It's a reference to the JsonObject, the actual bytes are inside the
-  // JsonBuffer with all the other nodes of the object tree.
-  // Memory is freed when jsonBuffer goes out of scope.
-  JsonObject& root = jsonBuffer.parseObject(jsonText);
-
-  // Test if parsing succeeds.
-  if (root.success())
-  {
-    Serial.print("\r\nParsing of JSON file succeded ...");
-  }
-  else
-  {
-    Serial.print("\r\nParsing of JSON file failed");
-    while(1);
-  }
-
-  // Print values
-  Serial.print("\r\n");
-  Serial.print("\r\nProject: ");
-  root["product"].printTo(Serial);
-  
-  Serial.print("\r\nVersion: ");
-  root["version"].printTo(Serial);
-  
-  Serial.print("\r\nBuild Date: ");
-  root["buildDate"].printTo(Serial);
-
-  Serial.print("\r\nFile Size: ");
-  Serial.print(myFileSize);
-    
-  JsonArray& tracks = root["tracks"];
-  int trackCount = tracks.size();
-
-  for (int i=0;i<trackCount;i++)
-  {
-    Serial.print("\r\n");
-    Serial.print("\r\nTrack Title: ");
-    tracks[i]["trackTitle"].printTo(Serial);
-    
-    Serial.print("\r\nTrack Start: ");
-    tracks[i]["startTime"].printTo(Serial);    
-  }
-  
-  Serial.print("\r\n");
-  Serial.print("\r\nDate: ");
+  //******************************  
 }
 
 void loop()
 {
+  //******************************
+  // Timing Actions
+  //******************************
+
+  if ( true == findNextFlag )
+  {
+    File myFile = SD.open("/config.json");
+    
+    // Get data from SD card, make sure file is not bigger than
+    //  the MAX_FILE_SIZE
+    int myFileSize = myFile.size();
+    
+    if ( myFileSize > MAX_FILE_SIZE)
+    {
+      Serial.print("\r\nFILE SIZE IS TOO LARGE!!!");
+      Serial.print("\r\nMAX FILE SIZE IS ");
+      Serial.print(MAX_FILE_SIZE);
+      Serial.print(" AND FILE SIZE IS ");
+      Serial.print(myFileSize);
+      while(1);  // don't do anything more    
+    }
+    
+    myFile.readBytes(jsonText, MAX_FILE_SIZE);
+    myFile.close();  
+  
+    // Memory pool for JSON object tree.
+    //
+    // Inside the brackets is the size of the pool in bytes.
+    // Don't forget to change this value to match your JSON document.
+    // Use arduinojson.org/assistant to compute the capacity.
+    StaticJsonBuffer<JSON_BUFFER_SIZE> jsonBuffer;
+  
+    // Root of the object tree.
+    //
+    // It's a reference to the JsonObject, the actual bytes are inside the
+    // JsonBuffer with all the other nodes of the object tree.
+    // Memory is freed when jsonBuffer goes out of scope.
+    JsonObject& root = jsonBuffer.parseObject(jsonText);
+  
+    // Test if parsing succeeds.
+    if (root.success())
+    {
+      Serial.print("\r\nParsing of JSON file succeded ...");
+    }
+    else
+    {
+      Serial.print("\r\nParsing of JSON file failed");
+      while(1);
+    }
+  
+    // Print values
+    Serial.print("\r\n");
+    Serial.print("\r\nProject: ");
+    root["product"].printTo(Serial);
+    
+    Serial.print("\r\nVersion: ");
+    root["version"].printTo(Serial);
+    
+    Serial.print("\r\nBuild Date: ");
+    root["buildDate"].printTo(Serial);
+  
+    Serial.print("\r\nFile Size: ");
+    Serial.print(myFileSize);
+      
+    JsonArray& tracks = root["tracks"];
+    int trackCount = tracks.size();
+  
+    for (int i=0;i<trackCount;i++)
+    {    
+      // FIXME, SET WHICH TRACK PLAYS NEXT HERE
+  
+      nextTime = 1525924370;
+      nextTrack = "track001.mp3";  
+  
+      // FIXME, SET WHICH TRACK PLAYS NEXT HERE
+    }
+    
+    Serial.print("\r\n");
+    Serial.print("\r\nNext Track Title: ");
+    Serial.print(nextTrack);
+    
+    Serial.print("\r\nNext Track Start: ");
+    Serial.print(nextTime);
+    Serial.print("\r\n");
+
+    findNextFlag = false;
+  }  
   
   //******************************
   // Timer Actions
@@ -280,7 +326,14 @@ void loop()
   {
     timerFlag = 0;
 
-    // DO SOMETHING
+    if ( now() >= nextTime )
+    {
+      /// Play a files  
+      Serial.println("\r\nPlaying Track 001");
+      musicPlayer.playFullFile("/track001.mp3");
+
+      findNextFlag = true;      
+    }
   }
   
   //******************************
@@ -328,7 +381,7 @@ void loop()
     Serial.print("  Fix: "); Serial.print((int)GPS.fix);
 
     Serial.print("  Epoch: ");
-    Serial.print(now());    
+    Serial.print( now() );    
   }
 }
 
